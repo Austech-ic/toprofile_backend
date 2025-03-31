72112350ee5d688197e89512e02e5bb23b669f2d
mathefagbe <fagbemi65@gmail.com>
2025-03-31 11:33:09 +0100

    fix

--- toprofile/settings.py
+++ toprofile/settings.py
@@ -47,6 +47,10 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     "rest_framework_simplejwt.token_blacklist",
+    "corsheaders",
+    "cloudinary_storage",
+    "cloudinary",
+
    
 
     "account",
